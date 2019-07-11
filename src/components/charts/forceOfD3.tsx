b00016ddcb21f97981f1ccfb56262f50f4fb4b63
jiangxiong <1372857406@qq.com>
2019-07-11 18:26:39 +0800

    力导向图 初步

--- src/components/charts/forceOfD3.tsx
+++ src/components/charts/forceOfD3.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from'./index.scss';
 import * as d3 from 'd3'
 
 interface IProps {
@@ -13,134 +14,208 @@ class ForceOfD3 extends React.Component<IProps, IState> {
     this.state = {
     }
   }
+
   componentDidMount() {
     const containerWidth = chartRef.parentElement.offsetWidth
     const data = this.props.data
-    const margin = { top: 30, right: 30, bottom: 30, left: 30 }
+    const margin = { top: 60, right: 60, bottom: 60, left: 60 }
     const width = containerWidth - margin.left - margin.right
     const height = 700 - margin.top - margin.bottom
-    let chart = d3
+    const chart = d3
       .select(chartRef)
       .attr('width', width + margin.left + margin.right)
       .attr('height', height + margin.top + margin.bottom)
-    let g = chart
+    const g = chart
       .append('g')
       .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')') // 设最外包层在总图上的相对位置
+
+    // 新建力导向图，以及一些配置
+    const simulation = d3
+      .forceSimulation() // 构建力导向图
+      .force(
+        'link',
+        d3
+          .forceLink()
+          .id(function(d, i) {
+            return i
+          })
+          .distance(function(d) {
+            return d.value * 50
+          })
+      )
+      .force('collision', d3.forceCollide(1).strength(0.1)) //
+      .force('charge', d3.forceManyBody())
+      .force('center', d3.forceCenter(width / 2, height / 2))
+
+    // 创建一个颜色的比例尺
     var colorScale = d3.scaleOrdinal()
-        .domain(d3.range(data.nodes.length))
-        .range(d3.schemeCategory10);
+      .domain(d3.range(data.nodes.length))
+      .range(d3.schemeCategory10);
 
-    //新建一个力导向图
-    var forceSimulation = d3.forceSimulation()
-        .force("link",d3.forceLink())
-        .force("charge",d3.forceManyBody())
-        .force("center",d3.forceCenter());;
+    let link = g
+      .append('g') // 画连接线
+      .attr('class', 'links')
+      .selectAll('line')
+      .data(data.edges)
+      .enter()
+      .append('line')
+      .attr("stroke",function(d,i){
+          return colorScale(i);
+      })
+      .attr("stroke-width",1);
 
-    //初始化力导向图，也就是传入数据
-    //生成节点数据
-    forceSimulation.nodes(data.nodes)
-        .on("tick",ticked);//这个函数很重要，后面给出具体实现和说明
-    //生成边数据
-    forceSimulation.force("link")
+    let linkText = g
+      .append('g') // 画连接连上面的关系文字
+      .attr('class', 'link-text')
+      .selectAll('text')
+      .data(data.edges)
+      .enter()
+      .append('text')
+      .text(function(d) {
+        return d.relation
+      })
+
+    let node = g
+      .append('g') // 画圆圈和文字`
+      .attr('class', 'nodes')
+      .selectAll('g')
+      .data(data.nodes)
+      .enter()
+      .append('g')
+      // .on('mouseover', function(d, i) {
+      //   //显示连接线上的文字
+      //   linkText.style('fill-opacity', function(edge) {
+      //     if (edge.source === d || edge.target === d) {
+      //       return 1
+      //     }
+      //   })
+      //   //连接线加粗
+      //   link
+      //     .style('stroke-width', function(edge) {
+      //       if (edge.source === d || edge.target === d) {
+      //         return '2px'
+      //       }
+      //     })
+      //     .style('stroke', function(edge) {
+      //       if (edge.source === d || edge.target === d) {
+      //         return '#000'
+      //       }
+      //     })
+      // })
+      // .on('mouseout', function(d, i) {
+      //   //隐去连接线上的文字
+      //   linkText.style('fill-opacity', function(edge) {
+      //     if (edge.source === d || edge.target === d) {
+      //       return 0
+      //     }
+      //   })
+      //   //连接线减粗
+      //   link
+      //     .style('stroke-width', function(edge) {
+      //       if (edge.source === d || edge.target === d) {
+      //         return '1px'
+      //       }
+      //     })
+      //     .style('stroke', function(edge) {
+      //       if (edge.source === d || edge.target === d) {
+      //         return '#ddd'
+      //       }
+      //     })
+      // })
+      .call(
+        d3
+          .drag()
+          .on('start', dragstarted)
+          .on('drag', dragged)
+          .on('end', dragended)
+      )
+
+    node
+      .append('circle')
+      .attr('r', 20)
+      .attr('fill', function(d, i) {
+        return colorScale(i)
+      })
+
+    node
+      .append('text')
+      .attr('fill', function(d, i) {
+        return colorScale(i)
+      })
+      .attr('y', -20)
+      .attr('dy', '.71em')
+      .text(function(d) {
+        return d.name
+      })
+
+    simulation // 初始化力导向图
+      .nodes(data.nodes)
+      .on('tick', ticked)
+
+    simulation.force('link')
         .links(data.edges)
         .distance(function(d){//每一边的长度
             return d.value*60;
         })
-    //设置图形的中心位置
-    forceSimulation.force("center")
-        .x(width/2)
-        .y(height/2);
-    //在浏览器的控制台输出
-    console.log(data.nodes);
-    console.log(data.edges);
 
-    //有了节点和边的数据后，我们开始绘制
-    //绘制边
-    var links = g.append("g")
-        .selectAll("line")
-        .data(data.edges)
-        .enter()
-        .append("line")
-        .attr("stroke",function(d,i){
-            return colorScale(i);
-        })
-        .attr("stroke-width",1);
-    var linksText = g.append("g")
-        .selectAll("text")
-        .data(data.edges)
-        .enter()
-        .append("text")
-        .text(function(d){
-            return d.relation;
-        })
+    chart
+      .append('g') // 输出标题
+      .attr('class', 'bar--title')
+      .append('text')
+      .attr('fill', '#000')
+      .attr('font-size', '16px')
+      .attr('font-weight', '700')
+      .attr('text-anchor', 'middle')
+      .attr('x', containerWidth / 2)
+      .attr('y', 20)
+      .text('人物关系图')
 
-    //绘制节点
-    //老规矩，先为节点和节点上的文字分组
-    var gs = g.selectAll(".circleText")
-        .data(data.nodes)
-        .enter()
-        .append("g")
-        .attr("transform",function(d,i){
-            var cirX = d.x;
-            var cirY = d.y;
-            return "translate("+cirX+","+cirY+")";
+    function ticked() {
+      // 力导向图变化函数，让力学图不断更新
+      link
+        .attr('x1', function(d) {
+          return d.source.x
         })
-        .call(d3.drag()
-            .on("start",started)
-            .on("drag",dragged)
-            .on("end",ended)
-        );
-
-    //绘制节点
-    gs.append("circle")
-        .attr("r",10)
-        .attr("fill",function(d,i){
-            return colorScale(i);
+        .attr('y1', function(d) {
+          return d.source.y
         })
-    //文字
-    gs.append("text")
-        .attr("x",-10)
-        .attr("y",-20)
-        .attr("dy",10)
-        .text(function(d){
-            return d.name;
+        .attr('x2', function(d) {
+          return d.target.x
         })
-
-    function ticked(){
-        links
-            .attr("x1",function(d){return d.source.x;})
-            .attr("y1",function(d){return d.source.y;})
-            .attr("x2",function(d){return d.target.x;})
-            .attr("y2",function(d){return d.target.y;});
-
-        linksText
-            .attr("x",function(d){
-            return (d.source.x+d.target.x)/2;
+        .attr('y2', function(d) {
+          return d.target.y
         })
-        .attr("y",function(d){
-            return (d.source.y+d.target.y)/2;
-        });
-
-        gs
-            .attr("transform",function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+      linkText
+        .attr('x', function(d) {
+          return (d.source.x + d.target.x) / 2
+        })
+        .attr('y', function(d) {
+          return (d.source.y + d.target.y) / 2
+        })
+      node.attr('transform', function(d) {
+        return 'translate(' + d.x + ',' + d.y + ')'
+      })
     }
-    function started(d){
-        if(!d3.event.active){
-            forceSimulation.alphaTarget(0.3).restart();
-        }
-        d.fx = d.x;
-        d.fy = d.y;
+
+    function dragstarted(d) {
+      if (!d3.event.active) {
+        simulation.alphaTarget(0.3).restart()
+      }
+      d.fx = d.x
+      d.fy = d.y
     }
-    function dragged(d){
-        d.fx = d3.event.x;
-        d.fy = d3.event.y;
+
+    function dragged(d) {
+      d.fx = d3.event.x
+      d.fy = d3.event.y
     }
-    function ended(d){
-        if(!d3.event.active){
-            forceSimulation.alphaTarget(0);
-        }
-        d.fx = null;
-        d.fy = null;
+
+    function dragended(d) {
+      // if (!d3.event.active) {
+      //   simulation.alphaTarget(0)
+      // }
+      // d.fx = null
+      // d.fy = null
     }
   }
 
